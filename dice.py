6c4a62282533d4725cbe245087af9cb65ba1d492
j-frances <jonathan.frances@live.com.au>
2020-08-20 09:47:42 +1000

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,22 @@
+from random import choice
+from sys import version_info
+if version_info.major == 3:
+    pass
+elif version_info.major == 2:
+    try:
+        input = raw_input
+    except NameError:
+        pass
+else:
+    print ("Unknown python version - input function not safe")
+
+options = [i for i in range(1,7)]
+print("Welcome to the dice game!")
+intention = input("Enter 'y' to roll!\n")
+while intention:
+    if intention == "y":
+        for _ in range(1):
+            roll = choice(options)
+            print("Nice! You got {}. Enter 'y' to roll again!".format(roll))
+            intention = input()
+            break
