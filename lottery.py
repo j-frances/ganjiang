ecdabaf722d5b94aa6192469c08ad9fb9f4a2c78
j-frances <jonathan.frances@live.com.au>
2020-08-22 17:46:26 +1000

    A simple game of chance
    Prompts the user to guess a random number generated between 0 and 100

---
+++ lottery.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+#print u'你猜怎么着?'.encode('utf-8')
+import os
+from random import randint
+from sys import version_info
+
+if version_info.major == 3:
+    pass
+elif version_info.major == 2:
+    try:
+        input = raw_input
+    except NameError:
+        pass
+else:
+    print ("Unknown python version - input function not safe")
+
+result = []
+intention = 'y'
+options = [i for i in range(1,100)]
+clear = lambda: os.system('clear')
+
+for _ in range(1):
+    roll = randint(0, 100)
+    result.append("{}".format(roll))
+
+while intention:
+    if intention == "y":
+        guess = 0
+        print("What is your guess?")
+        while guess == 0:
+            try:
+                guess = int(input())
+            except ValueError:
+                print("Please input your guess as a whole number.")
+                guess = 0
+                break
+        else:
+            if int(result[0]) == guess:
+                print("Correct!")
+                exit()
+            else:
+                print("You guessed {} but the right answer was {}. Sorry!".format(guess, result[0]))
+                intention = input("Enter 'y' to play again or 'q' to quit!.\n")
+    else:
+        if intention == "q":
+            clear()
+            exit()
+        else:
+            intention = input()
